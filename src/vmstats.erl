c6ed609461579330029d2d221bb9e2e8dc38cd5b
Pedram Nimreezi <deadzen@deadzen.com>
2013-11-01 15:06:22 -0400

    Add start/0 to vmstats

--- src/vmstats.erl
+++ src/vmstats.erl
@@ -2,9 +2,14 @@
 -behaviour(application).
 
 -export([
+    start/0,
     start/2,
     stop/1
 ]).
+
+start() ->
+    ok = application:start(statsderl),
+    ok = application:start(vmstats).
 
 start(normal, []) ->
     vmstats_sup:start_link("vmstats").
